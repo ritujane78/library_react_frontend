a7d372d9fff8a59555e0551b22971e3135001ea8
Ritu Bafna <ritujane78@gmail.com>
2025-10-09 16:03:37 +0545

    refactor homepage code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/Homepage/ExploreTopBooks';
-import { Carousel } from './layouts/Homepage/Carousel';
-import { Heros } from './layouts/Homepage/Heros';
-import { LibraryServices } from './layouts/Homepage/LibraryServices';
-import { Footer } from './layouts/NavbarAndFooter/Footer';
 
-function App() {
+import { LibraryServices } from './layouts/Homepage/components/LibraryServices';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { Homepage } from './layouts/Homepage/Homepage';
+
+export const App = () => {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibraryServices />
+      <Homepage />
       <Footer />
     </div>
 
   );
 }
-
-export default App;
---
+++ src/layouts/Homepage/Homepage.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "../NavbarAndFooter/Navbar";
+import { Carousel } from "./components/Carousel";
+import { ExploreTopBooks } from "./components/ExploreTopBooks";
+import { Heros } from "./components/Heros";
+import { LibraryServices } from "./components/LibraryServices";
+
+export const Homepage = () => {
+    return (
+        <>
+              <ExploreTopBooks />
+              <Carousel />
+              <Heros />
+              <LibraryServices />
+        </>
+    );
+}
